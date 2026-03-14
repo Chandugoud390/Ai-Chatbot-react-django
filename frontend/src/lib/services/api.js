70151e15922b44d2fc2dcc1e564b429bd80e9752
Chandu Goud <chandugoud0726@gmail.com>
2026-03-14 15:44:07 +0530

    fix api url

--- frontend/src/lib/services/api.js
+++ frontend/src/lib/services/api.js
@@ -4,7 +4,7 @@ export async function promptOpenai(data) {
   try {
     const response = await axios.post(
       // "http://localhost:8000/ai/chat_with_unihelp/",
-      "https://ai-chatbot-backend-9c6o.onrender.com/ai/chat_with_unihelp/",
+      "https://ai-chatbot-backend-9c6o.onrender.com/ai/chat_with_unihelp",
       data
     )
     return response.data
