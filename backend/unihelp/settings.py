a9b3191e8b892bb71729b25a910adbfd48b905a6
Chandu Goud <chandugoud0726@gmail.com>
2026-03-15 08:53:09 +0530

    fix static path

--- backend/unihelp/settings.py
+++ backend/unihelp/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/assets/'
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
